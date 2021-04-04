271954a9524553be5e227e0bf4dedde9136c793c
Lindineu Lopes Duran <37485332+LindineuDuran@users.noreply.github.com>
2021-04-04 10:14:48 -0300

    Introdução ao JPA e Hibernate
    - Adicionando JPA e configurando o Data Source;
    - Mapeando entidades com JPA;

--- algafood-api/src/main/java/com/lduran/algafood/AlgafoodApiApplication.java
+++ algafood-api/src/main/java/com/lduran/algafood/AlgafoodApiApplication.java
@@ -1,4 +1,4 @@
-package com.lduran.algafoodapi;
+package com.lduran.algafood;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- algafood-api/src/main/java/com/lduran/algafoodapi/injdep/notificacao/NivelUrgencia.java
+++
@@ -1,6 +0,0 @@
-package com.lduran.algafoodapi.injdep.notificacao;
-
-public enum NivelUrgencia
-{
-	URGENTE, SEM_URGENCIA
-}
