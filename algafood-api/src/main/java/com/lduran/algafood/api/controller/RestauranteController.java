606bebd32153f7054af19b1a2989773a017ab97e
Lindineu Lopes Duran <37485332+LindineuDuran@users.noreply.github.com>
2021-05-16 20:52:29 -0300

    REST com Spring
    - Atualização parcial com a API de Reflections do Spring

--- algafood-api/src/main/java/com/lduran/algafood/api/controller/RestauranteController.java
+++ algafood-api/src/main/java/com/lduran/algafood/api/controller/RestauranteController.java
@@ -1,12 +1,16 @@
 package com.lduran.algafood.api.controller;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -14,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lduran.algafood.domain.exception.EntidadeEmUsoException;
 import com.lduran.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.lduran.algafood.domain.model.Restaurante;
@@ -78,6 +83,51 @@ public class RestauranteController
 		{
 			return ResponseEntity.badRequest().body(e.getMessage());
 		}
+	}
+
+	@PatchMapping("/{restauranteId}")
+	public ResponseEntity<?> atualizarParcial(@PathVariable Long restauranteId, @RequestBody Map<String, Object> campos)
+	{
+		try
+		{
+			Restaurante restauranteAtual = cadastroRestaurante.buscar(restauranteId);
+
+			if (restauranteAtual == null)
+			{
+				return ResponseEntity.notFound().build();
+			}
+
+			merge(campos, restauranteAtual);
+
+			return atualizar(restauranteId, restauranteAtual);
+
+		}
+		catch (EntidadeNaoEncontradaException e)
+		{
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+	}
+
+	/**
+	 * @param dadosOrigem
+	 */
+	private void merge(Map<String, Object> dadosOrigem, Restaurante restauranteDestino)
+	{
+		ObjectMapper objectMapper = new ObjectMapper();
+		Restaurante restauranteOrigem = objectMapper.convertValue(dadosOrigem, Restaurante.class);
+		System.out.println(restauranteOrigem);
+
+		dadosOrigem.forEach((nomePropriedade, valorPropriedade) ->
+		{
+			Field campo = ReflectionUtils.findField(Restaurante.class, nomePropriedade);
+			campo.setAccessible(true);
+
+			Object novoValor = ReflectionUtils.getField(campo, restauranteOrigem);
+
+			System.out.println(nomePropriedade + "=" + valorPropriedade + "=" + novoValor);
+
+			ReflectionUtils.setField(campo, restauranteDestino, novoValor);
+		});
 	}
 
 	@DeleteMapping("/{restauranteId}")
