1b56e0ccdea7d82b1d7f1589b79b1ce0fb8a8132
Lindineu Lopes Duran <37485332+LindineuDuran@users.noreply.github.com>
2021-06-04 10:11:16 -0300

    Explorando mais do JPA e Hibernate
    - Desafio: mapeando relacionamento muitos-para-um
    - Desafio: mapeando relacionamento um-para-muitos
    - Desafio: mapeando relacionamentos muitos-para-muitos

--- algafood-api/src/main/java/com/lduran/algafood/domain/model/Restaurante.java
+++ algafood-api/src/main/java/com/lduran/algafood/domain/model/Restaurante.java
@@ -15,6 +15,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
@@ -63,4 +64,8 @@ public class Restaurante
 	@ManyToMany
 	@JoinTable(name = "restaurante_forma_pagamento", joinColumns = @JoinColumn(name = "restaurante_id"), inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
 	private List<FormaPagamento> formasPagamento = new ArrayList<>();
+
+	@JsonIgnore
+	@OneToMany(mappedBy = "restaurante")
+	private List<Produto> produtos = new ArrayList<>();
 }
