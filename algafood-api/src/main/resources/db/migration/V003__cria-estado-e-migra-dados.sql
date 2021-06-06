4832e1ccc0e904eb7d5c761eb654e786754017ef
Lindineu Lopes Duran <37485332+LindineuDuran@users.noreply.github.com>
2021-06-05 21:16:28 -0300

    Pool de conexões e Flyway
    - Adicionando o Flyway no projeto e criando a primeira migração
    - Evoluindo o banco de dados com novas migrações
    - Criando migrações complexas com remanejamento de dados
    - Criando migração a partir de DDL gerado por schema generation

---
+++ algafood-api/src/main/resources/db/migration/V003__cria-estado-e-migra-dados.sql
@@ -0,0 +1,24 @@
+CREATE TABLE estado (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(80) NOT NULL,
+    PRIMARY KEY (id)
+)  ENGINE=INNODB DEFAULT CHARSET=UTF8;
+
+insert into estado (nome) select distinct nome_estado from algafood.cidade;
+
+alter table cidade add column estado_id bigint not null;
+
+UPDATE cidade c 
+SET 
+    c.estado_id = (SELECT 
+            e.id
+        FROM
+            estado e
+        WHERE
+            e.nome = c.nome_estado);
+
+alter table cidade add constraint fk_cidade_estado foreign key (estado_id) references estado (id);
+
+alter table cidade drop column nome_estado;
+
+alter table cidade change nome_cidade nome varchar(60) not null;
