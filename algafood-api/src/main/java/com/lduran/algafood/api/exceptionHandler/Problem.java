303419553ee54752e8f30c81798298b1c0863346
Lindineu Lopes Duran <37485332+LindineuDuran@users.noreply.github.com>
2021-06-26 09:22:52 -0300

    Tratamento e modelagem de erros da API
    - Estendendo o formato do problema para adicionar novas propriedades
    - Desafio: estendendo o formato do problema

--- algafood-api/src/main/java/com/lduran/algafood/api/exceptionHandler/Problem.java
+++ algafood-api/src/main/java/com/lduran/algafood/api/exceptionHandler/Problem.java
@@ -1,5 +1,7 @@
 package com.lduran.algafood.api.exceptionHandler;
 
+import java.time.LocalDateTime;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
@@ -15,4 +17,7 @@ public class Problem
 	private String type;
 	private String title;
 	private String detail;
+
+	private String userMessage;
+	private LocalDateTime timestamp;
 }
