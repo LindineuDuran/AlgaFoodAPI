16802ecb1a24f7af136903fa4fa13d0b99358f2a
Lindineu Lopes Duran <37485332+LindineuDuran@users.noreply.github.com>
2021-07-10 19:53:36 -0300

    Testes de integração
    Criando e rodando um teste de integração com Spring Boot, JUnit e AssertJ

--- algafood-api/src/test/java/com/lduran/algafoodapi/CadastroCozinhaIntegrationTests.java
+++ algafood-api/src/test/java/com/lduran/algafoodapi/CadastroCozinhaIntegrationTests.java
@@ -1,17 +1,45 @@
 package com.lduran.algafoodapi;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import com.lduran.algafood.domain.model.Cozinha;
+import com.lduran.algafood.domain.repository.CozinhaRepository;
+import com.lduran.algafood.domain.service.CadastroCozinhaService;
+
 @ExtendWith(SpringExtension.class)
-@SpringBootTest(classes = { CadastroCozinhaIntegrationTests.class })
+@SpringBootTest
 public class CadastroCozinhaIntegrationTests
 {
+	@Autowired
+	private CozinhaRepository cozinhaRepository;
+
+	@Autowired
+	private CadastroCozinhaService cadastroCozinha;
+
 	@Test
 	public void contextLoads()
 	{
+		assertTrue(true);
+	}
 
+	@Test
+	public void testarCadastroCozinhaComSucesso()
+	{
+		// cenário
+		Cozinha novaCozinha = new Cozinha();
+		novaCozinha.setNome("Chinesa");
+
+		// ação
+		novaCozinha = cadastroCozinha.salvar(novaCozinha);
+
+//		// validação
+//		Assertions.assertThat(novaCozinha).isNotNull();
+//		Assertions.assertThat(novaCozinha.getId()).isNotNull();
 	}
 }
