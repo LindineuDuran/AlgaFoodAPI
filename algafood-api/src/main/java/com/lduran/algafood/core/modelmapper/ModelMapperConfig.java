c7566287d56039c872dd3fff52744f92654b0722
Lindineu Lopes Duran <37485332+LindineuDuran@users.noreply.github.com>
2021-10-30 17:32:04 -0300

    Boas práticas e técnicas para APIs
    - Revisando e ajustando as mensagens de validação com o uso de DTOs;
    - Estratégias de nomes de propriedades: snake case vs lower camel case

--- algafood-api/src/main/java/com/lduran/algafood/core/modelmapper/ModelMapperConfig.java
+++ algafood-api/src/main/java/com/lduran/algafood/core/modelmapper/ModelMapperConfig.java
@@ -4,9 +4,6 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.lduran.algafood.api.model.RestauranteModel;
-import com.lduran.algafood.domain.model.Restaurante;
-
 @Configuration
 public class ModelMapperConfig
 {
@@ -15,8 +12,8 @@ public class ModelMapperConfig
 	{
 		var modelMapper = new ModelMapper();
 
-		modelMapper.createTypeMap(Restaurante.class, RestauranteModel.class).addMapping(Restaurante::getTaxaFrete,
-				RestauranteModel::setPrecoFrete);
+//		modelMapper.createTypeMap(Restaurante.class, RestauranteModel.class).addMapping(Restaurante::getTaxaFrete,
+//				RestauranteModel::setPrecoFrete);
 
 		return modelMapper;
 	}
