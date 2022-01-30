5fd507c78a2604182a9f891f2590898d4352e216
Lindineu Lopes Duran <37485332+LindineuDuran@users.noreply.github.com>
2022-01-30 20:34:33 -0300

    CORS e consumo da API com JavaScript e Java
    - Implementando uma chamada na REST API com JavaScript;
    - Testando a requisição na API com JavaScript e entendendo a Same Origin Policy;
    - Entendendo o funcionamento básico de CORS e habilitando na API;

--- algafood-api/src/main/java/com/lduran/algafood/api/controller/RestauranteController.java
+++ algafood-api/src/main/java/com/lduran/algafood/api/controller/RestauranteController.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 
 import org.flywaydb.core.internal.util.ExceptionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -63,7 +64,9 @@ public class RestauranteController
 	@GetMapping
 	public ResponseEntity<List<RestauranteModel>> listarResumido()
 	{
-		return ResponseEntity.ok(assembler.toCollectionModel(cadastroRestaurante.listar()));
+		List<RestauranteModel> restaurantesModel = assembler.toCollectionModel(cadastroRestaurante.listar());
+		return ResponseEntity.ok().header(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "http://www.algafood.local:8000")
+				.body(restaurantesModel);
 	}
 
 	@JsonView(RestauranteView.ApenasNome.class)
---
+++ algafood-js-client/client.js
@@ -0,0 +1,14 @@
+function consultarRestaurantes()
+{
+    $.ajax({
+        url: "http://api.algafood.local:8080/restaurantes",
+        type: "get",
+
+        success: function(response)
+        {
+            $("#conteudo").text(JSON.stringify(response));
+        }
+    });
+}
+
+$("#botao").click(consultarRestaurantes);
